f41cf7f2f91dec620e568c82707febfd631bf81a
fedeschujman <fedeschujmanfede@gmail.com>
2023-04-26 22:28:21 -0300

    primer commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi primer app</h1>
+      <form className="formulario">
+        <p><input type="text" className="entrada" placeholder="Ingrese su Nombre" /></p>
+        <p><input type="text" className="entrada" placeholder="Ingrese su Apellido" /></p>
+        <input className="boton" type="submit" id="save" value="Guardar"/><br/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
